354bf48d552f243223c31c4aafaf3937cc281623
Tito <ejlopez44@gmail.com>
2020-03-27 23:17:32 -0400

    Implemented loop to generate all html elements

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # calendarAppHw
 Homework for Code Class to create a smart calendar application
 
+v3
+Created Loop to automatically clone the original hour block for an 8 hour work day
+- This could be taken out if unnecessary and hardcoded into the html doc
+
 v2
 Utilized Moment JS to display Day/Month/Date to the header
 
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
+$(document).ready(function () {
+    // This block populates today's date information to the header
+    var now = (moment().format('dddd[,] MMMM Do'))
+    $('#currentDay').text(now)
 
-// This block populates today's date information to the header
-var now = (moment().format('dddd[,] MMMM Do'))
-$('#currentDay').text(now)
+    var workDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+    //Created this as a way to systematically create each row, but 
+    function generateRows() {
+        for (let i = 1; i < workDay.length; i++) {
+            var hour = workDay[i]
+            var newRow = $('#hourRow').clone()
+            newRow.appendTo('#timeTable')
+            newRow.removeAttr('id', 'hourRow')
+            newRow.attr('id', 'hourRow' + [i])
+            // need to append <p> with text [i]
+            $('.hour').children().attr('id', 'hour' + hour)
+
+        }
+    }
+    generateRows()
+
+});
 
