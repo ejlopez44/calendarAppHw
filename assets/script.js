5716f7e06347e93ccc872e4bdf0d489777c394a4
Tito <ejlopez44@gmail.com>
2020-03-29 15:59:08 -0400

    Implemented functionality to store locally for each row independently

--- assets/script.js
+++ assets/script.js
@@ -7,37 +7,47 @@ $(document).ready(function () {
     // var currentHour = (moment().)
     $('#currentDay').text(now)
 
-    //need a function that saves input from specific textarea to local storage
-    //variables would be textarea id & name of local storage item
-
-    //need event listener on each button (by class?) that saves to dom of specific textarea id
+    // USEFUL FUNCTION FOR CONSOLING ATTRIBUTE OF A TARGETED ELEMENT
+    // console.log($('.time-block').attr('id'))
 
 
-    // your button needs to call a predetermined function...
-    $('button').on('click', tester);
+    //BEGIN LOCAL STORAGE CODE
 
-    function tester() {
-        //  needs to take the .val() of its previousSibling and save it to an array in local storage
+    //Needed to define an array to update items
+    var calEvents = ['', '', '', '', '', '', '', '']
 
-    }
-
+    //This function is run immediately to check for locally stored data and populate our calEvents array. Still trying to wrap my head around why it works..
+    getLocal()
     function getLocal() {
-        var saved = JSON.parse(localStorage.getItem('lastinput'))
+        var stored = JSON.parse(localStorage.getItem('Events'))
+        calEvents = stored
+        // console.log(saved)
+        console.log(calEvents)
     }
 
+    // THIS save FUNCTION IS WORKING AND SAVES VALUES ADDED TO THE INPUT BOX TO THE LOCAL STORAGE
     function save() {
-        var input = $(this).prev()
-        //consoles input to console for review
-        console.log(input.val())
-        //stores to local storage
-        localStorage.setItem('saved event', JSON.stringify(data));
+        //variable to help target the index of the row
+        var index = $(this).parent().index()
+        //variable to help target the textarea
+        var text = $(this).prev().val()
+        //this line tells me the index of the row in question 
+        console.log(index)
+        //this line tells me the current value typed in the textarea
+        console.log(text)
+
+        calEvents[index] = text
+
+        // // saves entire Object to local storage
+        localStorage.setItem("Events", JSON.stringify(calEvents));
     }
 
-    //Created this as a way to systematically create each row and save important object information
-    // var workDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-
+    // ARRAY FOR generateRows FUNCTION TO ITERATE THROUGH AND CREATE ELEMENTS
+    // var workDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"] // ORIGINAL SIMPLE ARRAY
     var workDay = [{ rowName: "9AM", hour: 9, input: "" }, { rowName: "10AM", hour: 10, input: "" }, { rowName: "11AM", hour: 11, input: "" }, { rowName: "12PM", hour: 12, input: "" }, { rowName: "1PM", hour: 13, input: "" }, { rowName: "2PM", hour: 14, input: "" }, { rowName: "3PM", hour: 15, input: "" }, { rowName: "4PM", hour: 16, input: "" }, { rowName: "5PM", hour: 17, input: "" }]
-    console.log(workDay)
+    // console.log(workDay)
+
+    // GENERATES HOUR ROWS
 
     function generateRows() {
         for (let i = 0; i < workDay.length; i++) {
@@ -53,9 +63,10 @@ $(document).ready(function () {
             var p = $('<p>').text(workDay[i].rowName);  // need to change this to target within the object: DONE
             //create textarea
             var textBlock = $('<textarea>')
+            textBlock.val(calEvents[i])
             //apply classes
             textBlock.addClass('col-8')
-            textBlock.attr("data-set", parseInt(workDay[i].hour)); // need to change this to get the 24 hour object hour and assign it
+            textBlock.attr("data-set", workDay[i].hour); // need to change this to get the 24 hour object hour and assign it
             //create button
             var saveButton = $('<button>')
             //apply classes
@@ -75,11 +86,9 @@ $(document).ready(function () {
 
         }
     }
-
     generateRows()
 
-
-    // console.log($('.time-block').attr('id'))
+    // FUNCTION CHANGES BG COLOR OF TEXT AREA
 
     function bgColor() {
         $('textarea').each(function () {
